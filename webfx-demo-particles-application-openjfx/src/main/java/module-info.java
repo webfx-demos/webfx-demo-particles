d2e606d2b59936cee757ada5d5e732d248361362
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-24 20:17:34 +0000

    Renamed appcontainer to boot in module names and java code

--- webfx-demo-particles-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-particles-application-openjfx/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module webfx.demo.particles.application.openjfx {
     // Direct dependencies modules
     requires webfx.demo.particles.application;
     requires webfx.kit.openjfx;
-    requires webfx.platform.java.appcontainer.impl;
+    requires webfx.platform.java.boot.impl;
     requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.shared.log.impl.simple;
