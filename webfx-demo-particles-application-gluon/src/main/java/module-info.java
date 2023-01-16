f8a98f1a5cc2e3dc8f9826496a1edffbc00ad226
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-16 17:04:57 +0000

    Ran 'webfx update' to fix GluonFX resourcesList pattern

--- webfx-demo-particles-application-gluon/src/main/java/module-info.java
+++ webfx-demo-particles-application-gluon/src/main/java/module-info.java
@@ -7,7 +7,7 @@ module webfx.demo.particles.application.gluon {
     requires webfx.kit.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
-    requires webfx.platform.resource.java;
+    requires webfx.platform.resource.gluon;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.gluon;
 
